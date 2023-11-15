0d7ff719ddcd73e7b81e0728e30e5dd8c765d4df
Myo Hsat <hsatmyo@gmail.com>
2023-11-15 11:15:23 +0630

    Second

---
+++ Piazza_shop_tailwindcss/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+   content: ["./index.html", "./javascript/*.js", "./components/*.{html,js}"],
+   theme: {
+      extend: {
+         fontFamily: {
+            brand: ["DynaPuff"],
+            body: ["Nunito"],
+            title: ["Roboto Slab"],
+         },
+      },
+   },
+   plugins: [],
+};
